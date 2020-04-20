1c4c9107938e546f106a2546c5e89efe27bb7239
David Riera <david.riera@somenergia.coop>
2020-04-20 11:56:12 +0200

    added extra prop content on tabs component

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -1,59 +1,68 @@
 import React, { useState } from 'react'
 import styled from 'styled-components'
 
+const TabBarWrapper = styled.div`
+display: flex;
+align-items: center;
+`
+const TabList = styled.ul`
+border-bottom: 0;
+padding: 12px 8px;
+background: #fff;
+display: flex;
+list-style: none;
+margin-bottom: 0;
+`
+const Tab = styled.li`
+background-color: #f2f2f2;
+margin: 0 4px;
+padding: 0;
+button {
+  padding: 10px 16px;
+  color: #585857;
+  font-weight: bold;
+  background: transparent;
+  border: 0;
+}
+&.active {
+  background-color: #96b633;
+  button {
+    color: #fff !important;
+    background: transparent
+  }
+}
+`
+const TabContentWrapper = styled.div`
+  background-color: #fff;
+  padding: 0 16px 16px;
+`
+
 const defaultTabs = [[
   { title: 'Tab 1', content: <div>Tab 1 content</div> },
   { title: 'Tab 2', content: <div>Tab 2 content</div> },
   { title: 'Tab 3', content: <div>Tab 3 content</div> }
 ]]
 
-export default function Tabs ({ tabs = defaultTabs }) {
-  const [activeTab, setActiveTab] = useState(0)
+export default function Tabs ({ tabs = defaultTabs, extra, initialTab = 0 }) {
+  const [activeTab, setActiveTab] = useState(initialTab)
   const activeTabContent = () => tabs[activeTab]?.content
-
-  const Tabs = styled.ul`
-    border-bottom: 0;
-    padding: 16px 8px;
-    background: #fff;
-    display: flex;
-    list-style: none;
-    margin-bottom: 0;
-  `
-  const Tab = styled.li`
-    background-color: #f2f2f2;
-    margin: 0 4px;
-    padding: 8px;
-    button {
-      color: #585857;
-      font-weight: bold;
-      background: transparent;
-      border: 0;
-    }
-    &.active {
-      background-color: #96b633;
-      button {
-        color: #fff !important;
-        background: transparent
-      }
-    }
-  `
-  const TabContentWrapper = styled.div`
-    background-color: #fff;
-    padding: 0 16px 16px;
-  `
 
   return (
     <React.Fragment>
-      <Tabs role="tablist">
-        {tabs.map(({ title }, index) => (
-          <Tab key={index} className={activeTab === index ? 'active' : null}>
-            <button href="#" role="tab" onClick={() => { setActiveTab(index) }}>
-              {title}
-            </button>
-          </Tab>
-        ))
-        }
-      </Tabs>
+      <TabBarWrapper>
+        <TabList role="tablist">
+          {tabs.map(({ title }, index) => (
+            <Tab key={index} className={activeTab === index ? 'active' : null}>
+              <button href="#" role="tab" onClick={() => { setActiveTab(index) }}>
+                {title}
+              </button>
+            </Tab>
+          ))
+          }
+        </TabList>
+        { extra }
+      </TabBarWrapper>
+
       <TabContentWrapper>
         {activeTabContent()}
       </TabContentWrapper>
