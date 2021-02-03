7640fbfe097a4248c44a4ed6ed256f4bed4e161e
David Riera <david.riera@somenergia.coop>
2021-02-03 15:25:18 +0100

    deleted service mock data

--- src/services/DistributionByPeriodMock.js
+++
@@ -1,3 +0,0 @@
-const mockData = { distribucio_periodes: { perc_punta: 51, perc_vall: 49 } }
-
-export default mockData
--- src/services/DistributionByUserTypeMock.js
+++
@@ -1,9 +0,0 @@
-const mockData = {
-    "distribucio_tipus_us": {
-        "permanent": 8.4, 
-        "peak": 41.2, 
-        "regular": 50.4
-    }
-}
-
-export default mockData
