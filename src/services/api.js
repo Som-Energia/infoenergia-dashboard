906c1cbc2e794dc1f326f40f4c39cb8491f02169
David Riera <david.riera@somenergia.coop>
2021-02-12 12:43:23 +0100

    refactor api api_prefix

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,10 @@
 import axios from 'axios'
 
 const { API_BASE_URL } = window.config
+const API_PREFIX = 'InfoenergiaReport/data'
 
 export const getDailyProfile = async (contract, token) => {
-  const url = `${API_BASE_URL}/InfoenergiaReport/data/dailyprofile/${contract}`
+  const url = `${API_BASE_URL}/${API_PREFIX}/dailyprofile/${contract}`
   const headers = {
     Authorization: `token ${token}`,
     'Content-Type': 'application/json'
@@ -16,7 +17,7 @@ export const getDailyProfile = async (contract, token) => {
 }
 
 export const getDistributionByPeriod = async (contract, token) => {
-  const url = `${API_BASE_URL}/InfoenergiaReport/data/distributionbyperiod/${contract}`
+  const url = `${API_BASE_URL}/${API_PREFIX}/distributionbyperiod/${contract}`
   const headers = {
     Authorization: `token ${token}`,
     'Content-Type': 'application/json'
@@ -29,7 +30,7 @@ export const getDistributionByPeriod = async (contract, token) => {
 }
 
 export const getDistributionByTypeOfUse = async (contract, token) => {
-  const url = `${API_BASE_URL}/InfoenergiaReport/data/distributionbytypeofuse/${contract}`
+  const url = `${API_BASE_URL}/${API_PREFIX}/distributionbytypeofuse/${contract}`
   const headers = {
     Authorization: `token ${token}`,
     'Content-Type': 'application/json'
@@ -42,7 +43,7 @@ export const getDistributionByTypeOfUse = async (contract, token) => {
 }
 
 export const getWeeklyProfile = async (contract, token) => {
-  const url = `${API_BASE_URL}/InfoenergiaReport/data/weeklyprofile/${contract}`
+  const url = `${API_BASE_URL}/${API_PREFIX}/weeklyprofile/${contract}`
   const headers = {
     Authorization: `token ${token}`,
     'Content-Type': 'application/json'
@@ -55,7 +56,7 @@ export const getWeeklyProfile = async (contract, token) => {
 }
 
 export const getMonthsProfile = async (contract, token) => {
-  const url = `${API_BASE_URL}/InfoenergiaReport/data/monthsprofile/${contract}`
+  const url = `${API_BASE_URL}/${API_PREFIX}/monthsprofile/${contract}`
   const headers = {
     Authorization: `token ${token}`,
     'Content-Type': 'application/json'
@@ -68,7 +69,7 @@ export const getMonthsProfile = async (contract, token) => {
 }
 
 export const getSeasonalProfile = async (contract, token) => {
-  const url = `${API_BASE_URL}/InfoenergiaReport/data/seasonalprofile/${contract}`
+  const url = `${API_BASE_URL}/${API_PREFIX}/seasonalprofile/${contract}`
   const headers = {
     Authorization: `token ${token}`,
     'Content-Type': 'application/json'
