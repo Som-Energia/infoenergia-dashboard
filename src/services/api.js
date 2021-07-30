bcc8c0c497b2602f7d3e3087be0d0852fd03b2f1
Marta <marta.fernandez@somenergia.coop>
2021-07-30 14:13:10 +0200

    API_BASE_URL -> HEMAN_API_URL

--- src/services/api.js
+++ src/services/api.js
@@ -1,10 +1,10 @@
 import axios from 'axios'
 
-const { API_BASE_URL } = window.config
+const { HEMAN_API_URL } = window.config
 const API_PREFIX = 'InfoenergiaReport/data'
 
 export const getDailyProfile = async (contract, token) => {
-  const url = `${API_BASE_URL}/${API_PREFIX}/dailyprofile/${contract}`
+  const url = `${HEMAN_API_URL}/${API_PREFIX}/dailyprofile/${contract}`
   const headers = {
     Authorization: `token ${token}`,
     'Content-Type': 'application/json',
@@ -16,7 +16,7 @@ export const getDailyProfile = async (contract, token) => {
 }
 
 export const getDistributionByPeriod = async (contract, token) => {
-  const url = `${API_BASE_URL}/${API_PREFIX}/distributionbyperiod/${contract}`
+  const url = `${HEMAN_API_URL}/${API_PREFIX}/distributionbyperiod/${contract}`
   const headers = {
     Authorization: `token ${token}`,
     'Content-Type': 'application/json',
@@ -28,7 +28,7 @@ export const getDistributionByPeriod = async (contract, token) => {
 }
 
 export const getDistributionByTypeOfUse = async (contract, token) => {
-  const url = `${API_BASE_URL}/${API_PREFIX}/distributionbytypeofuse/${contract}`
+  const url = `${HEMAN_API_URL}/${API_PREFIX}/distributionbytypeofuse/${contract}`
   const headers = {
     Authorization: `token ${token}`,
     'Content-Type': 'application/json',
@@ -40,7 +40,7 @@ export const getDistributionByTypeOfUse = async (contract, token) => {
 }
 
 export const getWeeklyProfile = async (contract, token) => {
-  const url = `${API_BASE_URL}/${API_PREFIX}/weeklyprofile/${contract}`
+  const url = `${HEMAN_API_URL}/${API_PREFIX}/weeklyprofile/${contract}`
   const headers = {
     Authorization: `token ${token}`,
     'Content-Type': 'application/json',
@@ -52,7 +52,7 @@ export const getWeeklyProfile = async (contract, token) => {
 }
 
 export const getMonthsProfile = async (contract, token) => {
-  const url = `${API_BASE_URL}/${API_PREFIX}/monthsprofile/${contract}`
+  const url = `${HEMAN_API_URL}/${API_PREFIX}/monthsprofile/${contract}`
   const headers = {
     Authorization: `token ${token}`,
     'Content-Type': 'application/json',
@@ -64,7 +64,7 @@ export const getMonthsProfile = async (contract, token) => {
 }
 
 export const getSeasonalProfile = async (contract, token) => {
-  const url = `${API_BASE_URL}/${API_PREFIX}/seasonalprofile/${contract}`
+  const url = `${HEMAN_API_URL}/${API_PREFIX}/seasonalprofile/${contract}`
   const headers = {
     Authorization: `token ${token}`,
     'Content-Type': 'application/json',
