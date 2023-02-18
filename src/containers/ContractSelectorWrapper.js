021e9c41800e0d5f4154c557d2de60b5ced3b6e0
David García Garzón <david.garcia@somenergia.coop>
2023-02-18 15:04:42 +0100

    conditionally show errors

--- src/containers/ContractSelectorWrapper.js
+++ src/containers/ContractSelectorWrapper.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 
+const empoweringToken = document.getElementById('root').dataset.token
 const contractList = JSON.parse(
   document.getElementById('contracts-data').textContent
 )
@@ -47,27 +48,19 @@ function ContractSelectorWrapper({ children }) {
           </div>
         </section>
         <section className="section infoenergy-container container">
-          {/* {% if empowering_token and contratos %} */}
-          <div>{children}</div>
-          {/* {% elif not contratos %} */}
-          <p className="warning">
-            {'trans Actualmente, no tiene contratos activos.'}
-          </p>
-          {/* {% else %} */}
-          <p className="warning">
-            {
-              'trans Su usuario no dispone de los datos necesarios para acceder a esta sección, puedes contactar con nosotros para mas información.'
-            }
-          </p>
-          {/* {% endif %} */}
-          <p style={{ display: 'none' }} id="no-data">
-            {'trans No hay curvas horarias para el contrato seleccionado'}.
-          </p>
-          <p style={{ display: 'none' }} id="no-allow">
-            {
-              'trans Su usuario no dispone de los datos necesarios para acceder a esta sección, puedes contactar con nosotros para mas información.'
-            }
-          </p>
+          {contracts.length === 0 ? (
+            <p className="warning" id="no-data">
+              {'trans Actualmente, no tiene contratos activos.'}
+            </p>
+          ) : !empoweringToken ? (
+            <p className="warning" id="no-allow">
+              {
+                'trans Su usuario no dispone de los datos necesarios para acceder a esta sección, puedes contactar con nosotros para mas información.'
+              }
+            </p>
+          ) : (
+            <div>{children}</div>
+          )}
           <p style={{ display: 'none' }} id="tarifa-30">
             {
               'trans Este contrato tiene una tarifa 3.0 con menos de 50 kW de potencia contratada. Las empresas de distribución no comparten los datos horarios de uso de energía de estos contratos, porque la normativa no les obliga a hacerlo. Por este motivo no disponemos de esta información y no podemos ofrecértela en este espacio.'
