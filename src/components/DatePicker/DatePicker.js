fa2c0d833f77080a8ebb25f566bb5c46beb6d989
dukenuken09 <gilgassorovira@gmail.com>
2023-10-06 13:06:59 +0200

    ✨ Added date picker for months and years

---
+++ src/components/DatePicker/DatePicker.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import MonthPicker from './MonthPicker'
+import YearPicker from './YearPicker'
+
+function DatePicker({ type, selectedDate, handleDateChange }) {
+/*   const [selectedDate, setSelectedDate] = useState(null)
+
+  const handleDateChange = (date, event) => {
+    event.preventDefault()
+    setSelectedDate(date)
+  } */
+
+  const addMonths = (months) => {
+    const newDate = new Date(selectedDate)
+    newDate.setMonth(newDate.getMonth() + months)
+    handleDateChange(newDate)
+  }
+  function subtractMonths(months) {
+    const newDate = new Date(selectedDate)
+    newDate.setMonth(newDate.getMonth() - months)
+    handleDateChange(newDate)
+  }
+
+  function addYears(years) {
+    const newDate = new Date(selectedDate)
+    newDate.setFullYear(newDate.getFullYear() + years)
+    handleDateChange(newDate)
+  }
+
+  function subtractYears(years) {
+    const newDate = new Date(selectedDate)
+    newDate.setFullYear(newDate.getFullYear() - years)
+    handleDateChange(newDate)
+  }
+
+  return (
+    <>
+      {type === 'month' ? (
+        <MonthPicker
+          add={addMonths}
+          substract={subtractMonths}
+          selectedDate={selectedDate}
+          handleDateChange={handleDateChange}
+        />
+      ) : (
+        <YearPicker
+          add={addYears}
+          substract={subtractYears}
+          selectedDate={selectedDate}
+          handleDateChange={handleDateChange}
+        />
+      )}
+    </>
+  )
+}
+
+export default DatePicker
