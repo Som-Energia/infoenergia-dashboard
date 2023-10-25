30a42164bc7cb3b8355ace635a571bc68b002ac5
dukenuken09 <gilgassorovira@gmail.com>
2023-10-25 14:39:36 +0200

    ✨ Added functions to format date, format data to consumption table, ordinals, etc

--- src/services/utils.js
+++ src/services/utils.js
@@ -154,14 +154,23 @@ export const groupMonthlyData = (data, tariffTimetableId) => {
   return month
 }
 
+<<<<<<< HEAD
 
+=======
+>>>>>>> ✨ Added functions to format date, format data to consumption table, ordinals, etc
 export const groupYearlyDataAccumulation = (data, tariffTimetableId) => {
   const base =
     tariffTimetableId === 'Taula_Peatges_20'
       ? {
+<<<<<<< HEAD
           PICK: 0,
           FLAT: 0,
           VALLEY: 0,
+=======
+          VALLEY: 0,
+          PICK: 0,
+          FLAT: 0,
+>>>>>>> ✨ Added functions to format date, format data to consumption table, ordinals, etc
         }
       : {
           P1: 0,
@@ -173,12 +182,20 @@ export const groupYearlyDataAccumulation = (data, tariffTimetableId) => {
         }
 
   const result = { ...base }
+<<<<<<< HEAD
   Object.keys(data).forEach((element) => {
     const period = getPeriod(element, tariffTimetableId)
     result[period] += data[element]
     result.value += data[element]
   })
 
+=======
+  for (let i = 0; i < data.length; i++) {
+    const period = getPeriod(data[i].date, tariffTimetableId)
+    result[period] += data[i].value
+    result.value += data[i].value
+  }
+>>>>>>> ✨ Added functions to format date, format data to consumption table, ordinals, etc
   return result
 }
 
@@ -416,7 +433,12 @@ export const CnmcformatData = ({ data, cups }) => {
 }
 
 export const CsvformatData = (data) => {
+<<<<<<< HEAD
   const formatedHeaders = data.columns.map((element) => ({
+=======
+
+  const formatedHeaders = data.columns.map(element => ({
+>>>>>>> ✨ Added functions to format date, format data to consumption table, ordinals, etc
     label: element,
     key: element,
   }))
