ae926664e37480908dd8b1de105d7607a6de852e
dukenuken09 <gilgassorovira@gmail.com>
2023-11-28 11:41:23 +0100

    ✨ Improve and generalize Generation rigths views

---
+++ src/containers/Generation/Record.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import RightsManage from 'components/Generation/RightsManage'
+
+function Record() {
+  
+
+  return (
+    <RightsManage>
+
+    </RightsManage>    
+  )
+}
+
+export default Record
