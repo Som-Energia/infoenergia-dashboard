2340bf6a90c94ec076b47ce7038bb2ddbc25acf1
dukenuken09 <gilgassorovira@gmail.com>
2025-01-13 12:54:31 +0100

    ✨ Add separation between mock and productive in investments api

--- src/services/api.js
+++ src/services/api.js
@@ -78,48 +78,13 @@ export const getSeasonalProfile = async (contract, token) => {
   })
 }
 
-export const getConsumption = async (date, token, type) => {
-  const urlY = '/api/investments/assignments-consumption-yearly/'
-  const urlM = '/api/investments/assignments-consumption-monthly/'
-
-  return axios({
-    method: 'GET',
-    url: type === 'month' ? urlM + date.format('YYYY-MM') : urlY +  date.year(),
-    headers: { Authorization: token },
-  }).then((response) => {
-    return response?.data
-  })
-}
-
-export const getkWhRecord = async (date, token) => {
-  return axios({
-    method: 'GET',
-    url: '/api/investments/assignments-kwh-rights/'+date.year(),
-    headers: { Authorization: token },
-  }).then((response) => {
-    return response?.data
-  })
-}
-
-export const getkWhRemaining = async (token) => {
-  return axios({
-    method: 'GET',
-    url: '/api/investments/assignments-remaining-kwh-production',
-    headers: { Authorization: token },
-  }).then((response) => {
-    return response?.data
-  }).catch((error) => {
-    console.log(error)
-    throw error
-  })
-}
-
-export const getLastInvoiceDatePriorityContract = async (token) => {
-  return axios({
-    method: 'GET',
-    url: '/api/investments/last-invoice-date-from-priority-contract',
-    headers: { Authorization: token },
-  }).then((response) => {
-    return response?.data
-  })
-}
+/* Choice development api or production api */
+const investmentsApi = investmentsFactApi.getApi(process.env.NODE_ENV)
+/* Use's table data */
+export const getConsumption = async (date, token, type) => investmentsApi.getConsumption(date, token, type)
+/* Production's chart data */
+export const getkWhRecord = async (date, token) => investmentsApi.getkWhRecord(date, token)
+/* Bag's chart data */
+export const getkWhRemaining = async (token) => investmentsApi.getkWhRemaining(token)
+/* Bag's chart last date */
+export const getLastInvoiceDatePriorityContract = async (token) => investmentsApi.getLastInvoiceDatePriorityContract(token)
---
+++ src/services/investmentsFactApi.js
@@ -0,0 +1,113 @@
+import axios from 'axios'
+
+const investmenAxios = axios.create({})
+
+
+const response = {
+    status: 200,
+    state: true,
+    headers: {}
+}
+
+export const getMockConsumption = async () => {
+    return Promise.resolve({
+        data: JSON.parse(process.env.REACT_APP_GENERATION_ASSIGNMENTS_USE_TABLE),
+        ...response
+    })
+}
+
+export const getMockKWhRecord = async () => {
+    return Promise.resolve({
+        data: JSON.parse(process.env.REACT_APP_GENERATION_PRODUCTION_CHART),
+        ...response
+    })
+}
+
+export const getMockKWhRemaining = async () => {
+    return Promise.resolve({
+        data: JSON.parse(process.env.REACT_APP_GENERATION_BAG_KWH),
+        ...response
+    })
+}
+
+export const getMockLastInvoiceDatePriorityContract = async () => {
+    return Promise.resolve({
+        data: JSON.parse(process.env.REACT_APP_GENERATION_LAST_INVOICE_DATE),
+        ...response
+    })
+}
+
+
+const devInvestmentApi = {
+    getConsumption: getMockConsumption,
+    getkWhRecord: getMockKWhRecord,
+    getkWhRemaining: getMockKWhRemaining,
+    getLastInvoiceDatePriorityContract: getMockLastInvoiceDatePriorityContract
+}
+
+
+const getConsumption = async (date, token, type) => {
+    const urlY = '/api/investments/assignments-consumption-yearly/'
+    const urlM = '/api/investments/assignments-consumption-monthly/'
+
+    return investmenAxios({
+        method: 'GET',
+        url: type === 'month' ? urlM + date.format('YYYY-MM') : urlY + date.year(),
+        headers: { Authorization: token, Identifier: "ass_consumption" },
+    }).then((response) => {
+        return response?.data
+    })
+}
+
+const getkWhRecord = async (date, token) => {
+    return investmenAxios({
+        method: 'GET',
+        url: '/api/investments/assignments-kwh-rights/' + date.year(),
+        headers: { Authorization: token, Identifier: "kwh_rights" },
+    }).then((response) => {
+        return response?.data
+    })
+}
+
+const getkWhRemaining = async (token) => {
+    return investmenAxios({
+        method: 'GET',
+        url: '/api/investments/assignments-remaining-kwh-production',
+        headers: { Authorization: token, Identifier: "kwh_production" },
+    }).then((response) => {
+        return response?.data
+    }).catch((error) => {
+        console.log(error)
+        throw error
+    })
+}
+
+const getLastInvoiceDatePriorityContract = async (token) => {
+    return investmenAxios({
+        method: 'GET',
+        url: '/api/investments/last-invoice-date-from-priority-contract',
+        headers: { Authorization: token, Identifier: "last_invoice_date" },
+    }).then((response) => {
+        return response?.data
+    })
+}
+
+
+const prodInvestmentApi = {
+    getConsumption: getConsumption,
+    getkWhRecord: getkWhRecord,
+    getkWhRemaining: getkWhRemaining,
+    getLastInvoiceDatePriorityContract: getLastInvoiceDatePriorityContract
+}
+
+
+const getApi = (mode) => {
+    if (mode === 'development') {
+        return devInvestmentApi
+    }
+    else {
+        return prodInvestmentApi
+    }
+}
+
+export default { getApi: getApi }
