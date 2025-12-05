e0f228002d2ae817a9af8b11de5d6eef456f4975
Mario <mario.martinez@somenergia.coop>
2025-12-05 12:05:38 +0100

    🎨 fix eslint with package and config updates

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,8 +3,22 @@ module.exports = {
     browser: true,
     es2020: true,
     node: true,
+    'jest/globals': true
   },
-  extends: ['plugin:react/recommended', 'standard', 'prettier'],
+  extends: [
+    'plugin:react/recommended',
+    'standard',
+    'prettier',
+    'plugin:jest/recommended'
+  ],
+  settings: {
+    react: {
+      version: '17'
+    },
+    jest: {
+      version: 29
+    }
+  },
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
@@ -12,9 +26,9 @@ module.exports = {
     ecmaVersion: 12,
     sourceType: 'module',
   },
-  plugins: ['react', 'prettier', "jest"],
+  plugins: ['react', 'prettier', 'jest'],
   rules: {
     'react/prop-types': 'off',
     'react/react-in-jsx-scope': 'off',
   },
-}
+};
