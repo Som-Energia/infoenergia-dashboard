41fefaf2d0a0513508f79f7cce603599bfe43f12
dukenuken09 <gilgassorovira@gmail.com>
2025-12-11 10:30:38 +0100

    ✨ Add configuration to build project as ov needed

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,34 +1,64 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'build', // CRA's default build output
-  },
-  server: {
-    open: true,
-    port: 3000
-  },
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    css: true,
-    setupFiles: './src/tests/setupTests.js',
 
-    // Replace problematic import with alias
-    alias: {
-      '^@mui/material/useMediaQuery$': '@mui/material/node/useMediaQuery/index.js',
+export default defineConfig(({ mode }) => {
+
+  const ovOptions = mode === 'ov' ? {
+    entryFileNames: 'static/js/main.js',
+    chunkFileNames: (fileInfo) => {
+      if (fileInfo.name.includes('vendor')) {
+        return 'static/js/vendor.js';  // Explicitly name the entry JS file
+      }
+      return 'static/js/[name]-[hash].js';
     },
+    assetFileNames: (assetInfo) => {
+      if (assetInfo.name.endsWith('.css')) {
+        return 'static/css/main.css'; // Explicitly name the CSS file
+      }
+      return 'static/css/[name]-[hash].[ext]';
+    }
+  } : {}
 
-    // Vitest MUI's package pre-bundle avoid internal module resolution problems
+
+  return {
+    plugins: [react()],
+    build: {
+      outDir: 'build', // CRA's default build output
+      manifest: 'asset-manifest.json',
+      cssCodeSplit: false,
+      sourcemap: true,
+      rollupOptions: {
+        output: {
+          ...ovOptions,
+        }
+      },
+      target: "es2020",
+    },
     server: {
-      deps: {
-        inline: [
-          '@mui/material',
-          '@mui/x-date-pickers',
-        ],
+      open: true,
+      port: 3000
+    },
+    test: {
+      globals: true,
+      environment: 'jsdom',
+      css: true,
+      setupFiles: './src/tests/setupTests.js',
+
+      // Replace problematic import with alias
+      alias: {
+        '^@mui/material/useMediaQuery$': '@mui/material/node/useMediaQuery/index.js',
+      },
+
+      // Vitest MUI's package pre-bundle avoid internal module resolution problems
+      server: {
+        deps: {
+          inline: [
+            '@mui/material',
+            '@mui/x-date-pickers',
+          ],
+        },
       },
     },
-  },
+  }
 });
