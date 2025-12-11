a8a6fd3c5ec9a68bd85cbc4940891ee8332f165d
Mario <mario.martinez@somenergia.coop>
2025-12-11 13:59:10 +0100

    🏗️ upgrade eslint and prettier configuration

--- src/tests/setupTests.js
+++ src/tests/setupTests.js
@@ -3,7 +3,8 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom'
-import 'mutationobserver-shim';
-import ResizeObserver from 'resize-observer-polyfill';
+import 'mutationobserver-shim'
+import ResizeObserver from 'resize-observer-polyfill'
 
-global.ResizeObserver = ResizeObserver;
+global.ResizeObserver = ResizeObserver
+
